7c0f135f069d0f1bf6ad235b6825b39523b2a20f
Fwalt237 <patrickkamwa17@gmail.com>
2025-03-25 19:15:57 +0100

    NIO_TASK

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Path pathFile = Paths.get(file.getAbsolutePath());
+        try{
+            List<String> lines = Files.readAllLines(pathFile);
+            String[] name = lines.get(0).split(": ");
+            String[] age = lines.get(1).split(": ");
+            String[] email = lines.get(2).split(": ");
+            String[] phone = lines.get(3).split(": ");
+            return new Profile(name[1], Integer.valueOf(age[1]), email[1], Long.valueOf(phone[1]));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
